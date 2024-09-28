71f5d8c925da8f9a8dc95e760a6f5c26b36dd42b
xavierrazz <sovannara1863@gmail.com>
2024-09-28 23:28:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function() {
+    document.getElementById('response').innerText = 'Love Has Rany Dalis too! ❤️';
+});
+
+document.getElementById('no').addEventListener('click', function() {
+    document.getElementById('response').innerText = 'anh ot kvol!';
+});
