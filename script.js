232eb2c65d29dc3b02cfe6eab35cbc0f8d42f258
xavierrazz <sovannara1863@gmail.com>
2024-09-29 22:26:08 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yes').addEventListener('click', function() {
-    document.getElementById('response').innerText = 'Love Has Rany Dalis too! ❤️';
-});
-
-document.getElementById('no').addEventListener('click', function() {
-    document.getElementById('response').innerText = 'anh ot kvol!';
-});
